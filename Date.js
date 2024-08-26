ce6e8d84507e5720fcc1a8c8ad96fdf2ebd94afe
Azrod60 <becerramathieu.60@gmail.com>
2024-08-26 11:31:37 +0200

    First commit

---
+++ Date.js
@@ -0,0 +1,12 @@
+function calculAge() {
+    const datenaissance = new Date(document.getElementById('datenaissance').value);
+    const today = new Date();
+    let age = today.getFullYear() - datenaissance.getFullYear();
+    const monthDiff = today.getMonth() - datenaissance.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < datenaissance.getDate())) {
+        age--;
+    }
+    
+    document.getElementById('result').textContent = `Vous avez ${age} ans.`;
+}
